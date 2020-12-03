0fc0acce2cba459526807a40ae0f58099a6b3afe
Alicia Weenum <weenum.alicia@gmail.com>
2020-12-02 22:25:13 -0500

    commit tree e22d5dec41c418acda35e57f0b9b68faec53ca51

---
+++ team.rb
@@ -0,0 +1,18 @@
+class Team < ActiveRecord
+    attr_accessor :name, :motto
+
+    @@all = []
+    def initialize(params)
+        @name =  params[:name]
+        @motto = params[:motto]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
+end
